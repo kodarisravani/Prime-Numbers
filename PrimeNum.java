1a7e2de51e660bad8f44833293d06a6e933a8d3f
kodarisravani <kodarisravani3@gmail.com>
2025-01-29 05:58:11 +0530

    Add files via upload

---
+++ PrimeNum.java
@@ -0,0 +1,22 @@
+public class PrimeNum{
+        public static void main(String[] args) {
+            int num = 23;
+            if (isPrime(num)) {
+                System.out.println(num + " is a prime number");
+            } else {
+                System.out.println(num + " is not a prime number");
+            }
+        }
+    
+        public static boolean isPrime(int num) {
+            if (num <= 1) {
+                return false;
+            }
+            for (int i = 2; i <= Math.sqrt(num); i++) {
+                if (num % i == 0) {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }  
